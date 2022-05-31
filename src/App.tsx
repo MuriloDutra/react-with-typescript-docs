eb485d860a45aa04aa4d2139617468dc614800e7
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2022-05-31 15:05:58 -0300

    Passing custom type to useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
+import { useState } from "react";
+import { User } from "./interfaces/User";
+
 interface AppProps {
   headerText: string;
   extraText?: string;
   subText?: string;
 }
 
-function App({ headerText, extraText, subText = "subtext here" }: AppProps) {
+function App({ headerText, extraText, subText = "How are you?" }: AppProps) {
+  const [user, setUser] = useState<User | null>(null);
+
+  function fetchUser(): void {
+    setUser({
+      name: "Murilo",
+      age: 21,
+      country: "Brazil",
+      address: {
+        street: "Main street",
+        number: 88,
+        zip: "13044502",
+      },
+      admin: false,
+    });
+  }
+
   return (
     <>
       <h1>{headerText}</h1>
       {extraText && <p>{extraText}</p>}
       {subText && <p>{subText}</p>}
+
+      <button onClick={fetchUser}>Fetch user on click</button>
+      {user && <p>Hello, {user.name}!</p>}
     </>
   );
 }
